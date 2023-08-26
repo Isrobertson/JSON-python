226fc4636336a188dcc552b0af640e44f6fc54d9
Ian Robertson <isrobertson21@gmail.com>
2023-08-26 17:46:29 -0400

    changed data to string type for json data later

--- JSON-manager.py
+++ JSON-manager.py
@@ -1,12 +1,18 @@
 import json
 
+limit = 0
+data = ''
+print('how many students are there?')
+limit = input()
+
+
 # test data (needs fixing)
-for x in range(10):
+for x in limit:
     student = "student" + str(x)
-    data = [{"Student": student, "marks":10, "status": "Fall"}]
+    data += '[{"Student": ' + student + ', "marks":10, "status": "Fall"}]'\
 
 #testing
-for x in range(10):
+for x in limit:
     print(data)
 
 # fix later
