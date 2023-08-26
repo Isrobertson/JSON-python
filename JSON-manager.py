94164889a776dc7136d0ed89eb9c0f97fd6506d1
Ian Robertson <isrobertson21@gmail.com>
2023-08-26 17:36:00 -0400

    added some comment detail

--- JSON-manager.py
+++ JSON-manager.py
@@ -1,6 +1,6 @@
 import json
 
-# test data
+# test data (needs fixing)
 for x in range(10):
     student = "student" + str(x)
     data = [{"Student": student, "marks":10, "status": "Fall"}]
@@ -9,6 +9,7 @@ for x in range(10):
 for x in range(10):
     print(data)
 
+# fix later
 def write_json(data, filename="1_new_json_data.json"):
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
