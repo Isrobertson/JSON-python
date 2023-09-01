525e8b326d605b613f642f52f7b3e9a220138aeb
Ian Robertson <isrobertson21@gmail.com>
2023-08-31 21:04:29 -0400

    fixed leading header data for "clients"

--- JSON-manager.py
+++ JSON-manager.py
@@ -1,7 +1,7 @@
 import json
 import csv
 
-#test read .csv file
+#read csv file
 with open('in/testsheet 1.csv', 'r') as read_csv_data:
     reader = csv.reader(read_csv_data)
     headers = next(reader) # Read the header row in csv file
@@ -13,26 +13,9 @@ with open('in/testsheet 1.csv', 'r') as read_csv_data:
             record[headers[i]] = value
         csvData.append(record)
 
-print(csvData)
+testData = {}
+testData['clients'] = csvData # this works!
 
-choice = input('would you like to create a .json file? (y / n) ')
-
-if choice == 'y' or choice == 'Y':
-    print('ok.')
-elif choice == 'n' or choice == 'N':
-    print('goodbye.')
-else:
-    print('error')
-
-
-data = {}
-#limit = 0
-
-#temporary data
-data['isrobertson'] = {'name': 'Ian',
-                       'address': 'NA',
-                       'Phone': '123456789'}
-
-# creates and sorts data into json file
-with open("test.json", 'w') as json_data:
-    json.dump(data, json_data, indent=4)
+# writes data into json file
+with open("out/test.json", 'w') as json_data:
+    json.dump(testData, json_data, indent=4)
